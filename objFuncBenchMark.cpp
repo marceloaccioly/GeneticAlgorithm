58cce6a57531e3e98b4baabc32e29eb5f26bc4c5
Marcelo <marcelo.accioly@icloud.com>
2021-10-28 19:30:54 -0300

    alteração metodo sphere

--- objFuncBenchMark.cpp
+++ objFuncBenchMark.cpp
@@ -1,7 +1,19 @@
 #include <algorithm>
 #include <cmath>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void costFunc(float x){
-    float y = 0;
-    y = __cpp_lambdas
+float costFunc(float x[]){
+    float _total = 0;
+    int len;
+    len = sizeof(x);
+    for (int i = 0; i <= len; i++)
+    {
+        /* code */
+        _total = pow(x[i],2);
+    }
+
+
+    return _total;
 }
